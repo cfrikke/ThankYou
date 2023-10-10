7cd416f9f940bbfbdf66f18065b71be0c2f17826
Coco <143611879+cfrikke@users.noreply.github.com>
2023-10-10 09:28:20 -0400

    Update code.js

--- RWD/code.js
+++ RWD/code.js
@@ -1,4 +1,7 @@
 function submitName() {
 let name = document.getElementById("name").value;
 document.getElementById("nameuse").innerHTML = ("name");
-  document.getElementById(
+  document.getElementById("name").classList.ToggleClass("hide");
+  document.getElementById("submit").classList.ToggleClass("hide");
+  document.getElementById("thanks").classList.ToggleClass("hide");
+  document.getElementById("nameuse").classList.ToggleClass("hide");
